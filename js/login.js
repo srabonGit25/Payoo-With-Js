2ca4972a9c341f54e5763a51e96945539ac08816
srabonGit25 <tasrabon25@hmail.com>
2025-03-07 04:49:56 +0600

    Second commit home page design

--- js/login.js
+++ js/login.js
@@ -2,5 +2,14 @@ document.getElementById('button-login').addEventListener('click', function(event
     event.preventDefault();  // <---------- problem for beginners to understand this
 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    
+    // validation example: without server
+    if( phoneNumber === '5' && pinNumber === '1234'){
+        console.log('logged in')
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('Wrong Phone Number or Pin');
+    }
 })
