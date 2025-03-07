c11bfac8fa3d4aa7db9791a2c0b384a067d7e243
srabonGit25 <tasrabon25@hmail.com>
2025-03-07 06:45:03 +0600

    verify pin number, string to number convert, update the balance in UI

--- js/home.js
+++ js/home.js
@@ -4,4 +4,20 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     event.preventDefault();
     const addMoneyInput = document.getElementById('input-add-money').value;
     const pinNumberInput = document.getElementById('input-pin-number').value;
+
+    // verfiy pin number & this is not right way
+    if(pinNumberInput === '1234'){
+
+        const balance = document.getElementById('account-balance').innerText;
+        // string to number convert
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        
+        // updste the balance in the ui/dom
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('Wrong Pin Number, Please Try Again');
+    }
 })
