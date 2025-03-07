b25d87e4ca3faf849ac66d3087c2c74f7ac365d8
srabonGit25 <tasrabon25@hmail.com>
2025-03-07 06:21:33 +0600

    third commit home page connected with js and link add money btn to amount input field

---
+++ js/home.js
@@ -0,0 +1,7 @@
+// add money to the account
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    // prevent page reload after form submit
+    event.preventDefault();
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+})
